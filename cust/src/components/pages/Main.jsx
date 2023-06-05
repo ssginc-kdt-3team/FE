6168251e72728149fdd696ffba32303f964b3ee5
juhyunn1 <21720819@yu.ac.kr>
2023-06-05 19:26:09 +0900

    chore : customer_modify authentication

--- cust/src/components/pages/Main.jsx
+++ cust/src/components/pages/Main.jsx
@@ -4,25 +4,10 @@ import { useSetRecoilState } from 'recoil';
 import { loginState } from '../../state/loginState';
 
 function Main() {
-  const setLoginState = useSetRecoilState(loginState);
-
-  const handleLogout = () => {
-    setLoginState({
-      id: -1,
-      isLoggedin: false
-    });
-  }
-
   return (
     <>
       메인화면
-      <button type='button' onClick={handleLogout}>로그아웃</button>
     </>
-    // <ul>
-    //   <li><Link to={"/resv/add"}>예약하기</Link></li>
-    //   <li><Link to={"/resv"}>예약내역</Link></li>
-    //   <li><Link to={"/resv/active"}>유효한 예약내역</Link></li>
-    // </ul>
   );
 }
 
